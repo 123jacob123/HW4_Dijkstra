c55fc42bc9bcae3a3a4545bde4902b09d9326fe2
123jacob123 <jacobbair7838@gmail.com>
2025-11-21 17:30:08 -0500

    In-progress search, pre-refactor

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
-
+import sys
+import heapq
 
 class Graph:
     def __init__(self):
-        #nodes contain all roads from themself
+        #nodes contain all roads from themselves
         self.nodes = {}
         self.places = {}
     
@@ -23,7 +24,7 @@ class Graph:
             self.add_node(ID2)
             self.nodes[ID2].update({ID1: weight})
 
-    #TODO: write tests to check graph is populated correctly
+
     def gen(self, placesFile, roadsFile):
         """
         method to read data into graph
@@ -35,18 +36,79 @@ class Graph:
             for line in file:
                 line = line.strip()
                 values = line.split(',')
-                self.places.update({values[0]: values[1]})
-                self.add_node(values[0])      
+                self.places.update({values[1]: values[0]})     
 
         with open(roadsFile, 'r') as file:
             for line in file:
                 line = line.strip()
                 values = line.split(",")
-                self.add_edge(values[0], values[1], values[2])
+                #weight multiplied by 100 so it can be int
+                self.add_edge(float(values[0]), float(values[1]), float(values[2]))
 
     #TODO: implement Dijsktras
     def getPath(self, srcName, destName):
-        print("null")
+        srcID = int(self.places.get(srcName))
+        destID = int(self.places.get(destName))
+
+        pq = []
+        dist = [sys.maxsize] * len(self.nodes)
+        parent = {node: None for node in self.nodes}
+        
+        dist[self.getIndex(srcID)] = 0
+        heapq.heappush(pq, (0, srcID))
+
+        while pq:
+            d, u = heapq.heappop(pq)
+            if d > dist[u]:
+                continue
+
+            for v, w in self.nodes.get(u).items():
+                new_dist = dist[self.getIndex(u)] + w
+                if new_dist < dist[int(v)]:
+                    dist[v] = new_dist
+                    parent[v] = u
+                    heapq.heappush(pq, (new_dist, v))
+        
+        path = []
+        node = destID
+        while node is not None:
+            path.append(node)
+            node = parent[node]
+        path.reverse()
+
+        return path, dist, parent
+
+        """
+        while pq:
+            d, u = heapq.heappop(pq)
+
+            if d > dist[u]:
+                continue
+
+            print(self.nodes.get(u))
+            for v, w in self.nodes.get(u).items():
+                if dist[u] + float(w) < dist[self.getIndex(v)]:
+                    dist[v] = dist[u] + w
+                    heapq.heappush(pq, (dist[v], v))
+        print(dist[5])
+        """
+
+    
+    def getIndex(self, target):
+        """
+        input: node ID
+        return: index of node in places and dist
+        """
+        index = 0
+        for ID in self.nodes:
+            if target == int(ID):
+                return index
+            index+=1
+
+
+
+        
+
 
 
 
@@ -72,12 +134,13 @@ class UnitTest:
             for line in file:
                 line = line.strip()
                 values = line.split(",")
-                self.roadList.append(values)
+                self.roadList.append([float(values[0]), float(values[1]), float(values[2])])
 
     def ranRouteCheck(self, n=5):
         """
         Randomly selects 5 roads from the roadlist and test
         that the route is present on the src node
+        Returns count of road misses
         """
         count = 0
         err = 0
@@ -97,9 +160,24 @@ class UnitTest:
 
 
 def main():
-    print("boob")
     test = UnitTest("USRoads/Place.txt", "USRoads/Road.txt")
     print("Graph gen errors: " + str(test.ranRouteCheck(5)))
+
+    graph = Graph()
+    graph.gen("USRoads/Place.txt", "USRoads/Road.txt")
+    path, dist, parent = graph.getPath("ALBRIDGEPORT", "MDWASHINGTON GROVE")
+
+    print("Path:", path)
+    print("Detailed steps:")
+    total = 0
+    for i in range(len(path) - 1):
+        u = path[i]
+        v = path[i+1]
+        w = graph[u][v]
+        print(f"{u} -> {v} (weight {w})")
+        total += w
+
+    print("Total distance:", total)
 
 
 if __name__ == "__main__":
