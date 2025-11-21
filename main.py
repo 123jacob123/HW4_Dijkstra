9a9a7cccce5fff037087964f39d223ab19af3b32
123jacob123 <jacobbair7838@gmail.com>
2025-11-21 14:25:10 -0500

    Graph Gen Unit Tests

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Graph:
     def __init__(self):
         #nodes contain all roads from themself
@@ -40,18 +43,63 @@ class Graph:
                 line = line.strip()
                 values = line.split(",")
                 self.add_edge(values[0], values[1], values[2])
-    
+
     #TODO: implement Dijsktras
     def getPath(self, srcName, destName):
         print("null")
-                
-    
+
+
+
+class UnitTest:
+    """
+    Usage: Create Unittest Object, use methods to run test
+    """
+    def __init__(self, places, roads):
+        """
+        Makes a gens a graph object
+        Generates a road list to test graph is populating correctly
+        """
+        self.graph = Graph()
+        self.graph.gen(places, roads)
+        self.roadList = []
+        self.genRoadList(roads)
+
+    def genRoadList(self, roads):
+        """
+        Populates road list with the complete set of routes
+        """
+        with open(roads, 'r') as file:
+            for line in file:
+                line = line.strip()
+                values = line.split(",")
+                self.roadList.append(values)
+
+    def ranRouteCheck(self, n=5):
+        """
+        Randomly selects 5 roads from the roadlist and test
+        that the route is present on the src node
+        """
+        count = 0
+        err = 0
+        while count < n:
+            testID = random.randint(0, len(self.roadList))
+            test = self.roadList[testID]
+            src = self.graph.nodes.get(test[0])
+            if (test[1] in src):
+                if src.get(test[1]) != test[2]:
+                    err+=1
+            else:
+                err+=1
+            count+=1
+        return err
+        
+
+
 
 def main():
     print("boob")
-    graph = Graph()
-    graph.gen("USRoads/Place.txt", "USRoads/Road.txt")
-    print(graph.nodes.get('396'))
+    test = UnitTest("USRoads/Place.txt", "USRoads/Road.txt")
+    print("Graph gen errors: " + str(test.ranRouteCheck(5)))
 
 
 if __name__ == "__main__":
